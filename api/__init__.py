ea1f318f350db54a519335ebdfcd4cc69b8808f5
mercurial <dias.nespayev@gmail.com>
2022-04-12 12:57:12 +0600

    chore: add flask_sqlalchemy

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
+
+    db.init_app(app)
 
     from api.auth import auth
     app.register_blueprint(auth, url_prefix='/auth')
