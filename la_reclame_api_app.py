8b8ab6786d26cd43c147327fb3dc58e0cfa3151c
mercurial <dias.nespayev@gmail.com>
2022-04-12 13:03:55 +0600

    chore: add migrations

--- la_reclame_api_app.py
+++ la_reclame_api_app.py
@@ -1,7 +1,10 @@
-from api import create_app
+from api import create_app, db
+from flask_migrate import Migrate
+
+from api.models import Users
 
 app = create_app()
-
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     app.run()
