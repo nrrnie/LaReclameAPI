e4565c05527dd2bdd1361536fe19d86373c50c3d
mercurial <dias.nespayev@gmail.com>
2022-04-12 13:16:37 +0600

    chore: add `email` field

--- api/models.py
+++ api/models.py
@@ -6,4 +6,5 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
     registered = db.Column(db.DATETIME, nullable=False, default=datetime.now)
