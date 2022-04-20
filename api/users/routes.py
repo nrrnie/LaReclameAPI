500e2af77d108d45467734ec977ed60e692cb5b1
nrrnie <fnrr.35@gmail.com>
2022-04-20 20:11:24 +0600

    feat: add user_by_username()

--- api/users/routes.py
+++ api/users/routes.py
@@ -16,3 +16,17 @@ def user_by_id(user_id: int):
 		'user': user.get_json()
 	}
 
+@users.route('/user_by_username/<user_name>', methods=['POST'])
+def user_by_username(user_name: str):
+	user = Users.query.filter_by(username=user_name).first()
+	
+	if user is None:
+		return {
+			'status': 'error',
+			'error': 'User with such username is not found.'
+		}
+
+	return {
+		'status': 'ok',
+		'user': user.get_json()
+	}
