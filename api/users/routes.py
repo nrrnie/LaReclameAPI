bc1daf182f88284dd95024135bbdc894161b4325
mercurial <dias.nespayev@gmail.com>
2022-04-21 06:09:27 +0600

    fix: add users blueprint

--- api/users/routes.py
+++ api/users/routes.py
@@ -1,47 +1,50 @@
 from api.users import users
 from api.models import Users
 
+
 @users.route('/user_by_id/<user_id>', methods=['POST'])
 def user_by_id(user_id: int):
-	user = Users.query.get(user_id)
+    user = Users.query.get(user_id)
 
-	if user is None:
-		return {
-			'status': 'error',
-			'error': 'User with such ID is not found.'
-		}
+    if user is None:
+        return {
+            'status': 'error',
+            'error': 'User with such ID is not found.'
+        }
 
-	return {
-		'status': 'ok',
-		'user': user.get_json()
-	}
+    return {
+        'status': 'ok',
+        'user': user.get_json()
+    }
+
 
 @users.route('/user_by_username/<user_name>', methods=['POST'])
 def user_by_username(user_name: str):
-	user = Users.query.filter_by(username=user_name).first()
-	
-	if user is None:
-		return {
-			'status': 'error',
-			'error': 'User with such username is not found.'
-		}
+    user = Users.query.filter_by(username=user_name).first()
 
-	return {
-		'status': 'ok',
-		'user': user.get_json()
-	}
+    if user is None:
+        return {
+            'status': 'error',
+            'error': 'User with such username is not found.'
+        }
+
+    return {
+        'status': 'ok',
+        'user': user.get_json()
+    }
+
 
 @users.route('/user_by_email/<user_email>', methods=['POST'])
 def user_by_email(user_email: str):
-	user = Users.query.filter_by(email=user_email).first()
+    user = Users.query.filter_by(email=user_email).first()
 
-	if user is None:
-		return {
-			'status': 'error',
-			'error': 'User with such email is not found.'
-		}
+    if user is None:
+        return {
+            'status': 'error',
+            'error': 'User with such email is not found.'
+        }
 
-	return {
-		'status': 'ok',
-		'user': user.get_json()
-	}
+    return {
+        'status': 'ok',
+        'user': user.get_json()
+    }
