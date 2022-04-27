e7b133f61a5bea43688ac47aafffa6ec60b6d066
mercurial <dias.nespayev@gmail.com>
2022-04-28 00:03:05 +0600

    fix: absolute path to images

--- api/utils/routes.py
+++ api/utils/routes.py
@@ -1,10 +1,11 @@
-from flask import request, send_file
+from flask import send_file
 from api.utils import utils
 import os
 
-@utils.route('/get-image/<filename>', methods=['POST'])
+@utils.route('/get-image/<filename>', methods=['GET', 'POST'])
 def get_image(filename: str):
-	path_to_project = os.path.abspath(__name__)
-	path_to_storage = '\\images\\'
+	path_to_project = os.path.split(os.path.abspath(__name__))[0]
+	path_to_storage = os.path.join(path_to_project, 'image_storage')
+	path_to_image = os.path.join(path_to_storage, filename)
 
-	return send_file(path_to_project + path_to_storage + filename)
+	return send_file(path_to_image)
